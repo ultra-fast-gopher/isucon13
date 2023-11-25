5826ea36f58b9f679e2be573325f892b7ab09772
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2023-11-25 11:44:05 +0900

    add themes_user_id

--- sql/index.sql
+++ sql/index.sql
@@ -3,3 +3,4 @@ ALTER TABLE `isupipe`.`livestreams` ADD INDEX `livestreams_user_id` (`user_id`);
 ALTER TABLE `isupipe`.`reactions` ADD INDEX `reactions_livestream_id` (`livestream_id`);
 ALTER TABLE `isupipe`.`livestream_tags` ADD INDEX `livestream_tags_livestream_id` (`livestream_id`);
 ALTER TABLE `isupipe`.`icons` ADD INDEX `icons_user_id` (`user_id`);
+ALTER TABLE `isupipe`.`themes` ADD INDEX `themes_user_id` (`user_id`);
