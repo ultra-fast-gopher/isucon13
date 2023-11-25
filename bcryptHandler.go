9328d02df4c1eaadee3e10d048bbdb38603f40ad
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-11-25 17:17:52 +0900

    Cache bcrypto

--- bcryptHandler.go
+++ bcryptHandler.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"golang.org/x/crypto/bcrypt"
-	"net/http"
 )
 
 type PostBcryptCompairHandler struct {
@@ -19,19 +20,34 @@ type PostBcryptSumResult struct {
 	HashedPassword string `json:"hashed_password"`
 }
 
+var cache Map[string, bool]
+
 func bcryptCompairHandler(c echo.Context) error {
 	req := new(PostBcryptCompairHandler)
 	if err := c.Bind(req); err != nil {
 		return err
 	}
 
+	equal, found := cache.Load(req.HashedPassword + ":" + req.Password)
+
+	if found {
+		if equal {
+			return c.NoContent(200)
+		} else {
+			return echo.NewHTTPError(http.StatusUnauthorized, "invalid username or password")
+		}
+	}
+
 	err := bcrypt.CompareHashAndPassword([]byte(req.HashedPassword), []byte(req.Password))
 	if err != nil {
 		if err == bcrypt.ErrMismatchedHashAndPassword {
+			cache.Store(req.HashedPassword+"-"+req.Password, false)
+
 			return echo.NewHTTPError(http.StatusUnauthorized, "invalid username or password")
 		}
 		return err
 	}
+	cache.Store(req.HashedPassword+"-"+req.Password, true)
 
 	return c.NoContent(200)
 }
